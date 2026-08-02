f54c3172afe9b01019f049c8f0754cc8a8e34961
Tom Houghton-Alliss <tomhoughton18@gmail.com>
2026-08-02 13:43:16 +0000

    Added some providers to app run time

--- src/app/runtime.ts
+++ src/app/runtime.ts
@@ -1,4 +1,10 @@
+import { MessageChannel } from "../application/message-channel/index.js";
+import { ModelProvider } from "../application/model-provider/index.js";
+import { TaskProvider } from "../application/task-provider/index.js";
 import { createHttpServer } from "../entrypoints/http/server-factory.js";
+import { TelegramMessageChannel } from "../infrastructure/message-channel/telegram/index.js";
+import { VercelAiSdkOpenAiModelProvider } from "../infrastructure/model-provider/openai/index.js";
+import { TodoistTaskProvider } from "../infrastructure/task-provider/todoist/index.js";
 import type { AppConfig } from "./config.js";
 
 /**
@@ -6,6 +12,9 @@ import type { AppConfig } from "./config.js";
  */
 export interface AppRuntime {
   server: ReturnType<typeof createHttpServer>;
+  aiProvider: ModelProvider;
+  messageProvider: MessageChannel;
+  taskProvider: TaskProvider;
 }
 
 /**
@@ -14,5 +23,19 @@ export interface AppRuntime {
 export function buildAppRuntime(config: AppConfig): AppRuntime {
   return {
     server: createHttpServer(config),
+    aiProvider: new VercelAiSdkOpenAiModelProvider({
+      apiKey: config.integrations.openai.apiKey,
+      baseUrl: config.integrations.openai.baseUrl,
+      model: config.integrations.openai.model,
+    }),
+    messageProvider: new TelegramMessageChannel({
+      apiBaseUrl: config.integrations.telegram.apiBaseUrl,
+      botToken: config.integrations.telegram.botToken,
+      chatId: config.integrations.telegram.chatId,
+    }),
+    taskProvider: new TodoistTaskProvider({
+      apiBaseUrl: config.integrations.todoist.apiBaseUrl,
+      apiToken: config.integrations.todoist.apiToken,
+    }),
   };
 }
